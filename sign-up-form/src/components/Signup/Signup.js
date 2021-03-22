e9660f7c3cbdf55f8006f140af54b9ac2782ef5b
ph4ntom5 <tomsstals1@gmail.com>
2021-03-22 14:41:46 +0100

    wip

--- sign-up-form/src/components/Signup/Signup.js
+++ sign-up-form/src/components/Signup/Signup.js
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import "./Signup.css";
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value,
+  };
+};
+
 export const Signup = () => {
+  const [setFormData] = useReducer(formReducer, {});
   const [submitting, setSubmitting] = useState(false);
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -9,6 +17,10 @@ export const Signup = () => {
     setTimeout(() => {
       setSubmitting(false);
     }, 2000);
+  };
+
+  const handleChange = (event) => {
+    setFormData({ name: event.target.name, value: event.target.value });
   };
   return (
     <div className="box">
@@ -22,10 +34,24 @@ export const Signup = () => {
         <form onSubmit={handleSubmit}>
           <fieldset>
             <label>
-              <input name="name" placeholder="First Name" />
+              <input
+                name="name"
+                placeholder="First Name"
+                onChange={handleChange}
+              />
             </label>
             <label>
               <input name="name" placeholder="Last Name" />
+            </label>
+            <label>
+              <input name="email" placeholder="Email Address" />
+            </label>
+            <label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+              ></input>
             </label>
             <button type="submit">CLAIM YOUR FREE TRIAL</button>
             <p className="terms">
