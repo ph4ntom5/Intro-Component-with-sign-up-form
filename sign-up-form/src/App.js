f1ad176064b8d6c6929eb39e99f3b52475b134b0
ph4ntom5 <tomsstals1@gmail.com>
2023-07-25 11:18:59 +0200

    supabase install

--- sign-up-form/src/App.js
+++ sign-up-form/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header.js";
 import Signup from "./components/Signup/Signup.js";
+import supabase from "../src/config/supabaseClient";
 
 function App() {
+  console.log(supabase);
   return (
     <div className="App">
       <Header />
